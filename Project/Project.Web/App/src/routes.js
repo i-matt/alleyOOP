e5cdab4b06f8e1ad740db974faf281362ae92fa7
Matt Ipavec <matt.ipavec@gmail.com>
2018-03-17 01:26:52 -0700

    register, login, and terms &conditions done

--- Project/Project.Web/App/src/routes.js
+++ Project/Project.Web/App/src/routes.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import App from '../src/views/App';
 import Register from '../src/views/Register';
-// import Login from '../src/views/Login';
-// import TermsAndConditions from '../src/Views/TermsAndConditions';
+import Login from '../src/views/Login';
+import TConditions from '../src/views/TConditions';
 import NavBar from '../src/common/navbar';
 // import Donations from '../src/Views/Donations';
 
@@ -11,9 +11,9 @@ const Routes = () => (
     <Router>
         <div id="topDiv">
             <Switch>
-                {/* <Route path="/login" component={Login}/> */}
+                <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register} />
-                {/* <Route path="/termsandconditions" component={TermsAndConditions} /> */}
+                <Route path="/terms" component={TConditions} />
                 <NavBar>
                     <Route exact path="/" component={App} />
                     <Route path="/app" component={App} />
